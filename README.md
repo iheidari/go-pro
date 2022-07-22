dd325a460d8a5b08ec1f37cf22c9a1f437f1f529
iheidari <iman.heydari@gmail.com>
2022-07-22 13:52:42 -0700

    fix concat

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ ffmpeg -ss 00:01:00 -to 00:02:00 -i input.mp4 -c copy output.mp4
 
 video to image
 ffmpeg -i input.mp4 -vf fps=1 out%d.png
+
+concat video
+ffmpeg -i ./data/${file} -c copy -bsf:v h264_mp4toannexb -f mpegts ./out/${file}.ts
+ffmpeg -i "concat:${\_FILES.join("|")}" -c copy -bsf:a aac_adtstoasc output.mp4
--- video-concat.js
+++ video-concat.js
@@ -1,31 +1,48 @@
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 const _FILES = [
-  "GH010023.MP4",
-  "GH010024.MP4",
-  "GH010025.MP4",
-  "GH010027.MP4",
-  "GH010028.MP4",
-  "GH010031.MP4",
+  "GH010351",
+  "GH010352",
+  // "GH010353",
+  // "GH010354",
+  // "GH010355",
+  // "GH010356",
+  // "GH010357",
 ];
+const _INPUT_FOLDER = "./files/";
+const _OUTPUT_FOLDER = "./out/";
+const _FINAL_RESULT = _OUTPUT_FOLDER + "final.mp4";
 
-// _FILES.forEach(async (file, index) => {
-//   console.log(`🚀 (${index + 1} of ${_FILES.length})file: ${file}`);
-//   await exec(
-//     `ffmpeg -i ./data/${file} -c copy -bsf:v h264_mp4toannexb -f mpegts ./out/${file}.ts`
-//   );
-// });
+const CONSOLE_RESET = "\x1b[0m";
+const CONSOLE_YELLOW = "\x1b[33m";
 
-console.log(
-  `ffmpeg -i "concat:${_FILES
-    .map((f) => "./out/" + f + ".ts")
-    .join("|")}" -c copy -bsf:a aac_adtstoasc ./data/GH0100.mp4`
-);
-// const concat = async () => {
-//   await exec(
-//     `ffmpeg -i "concat:${_FILES.join(
-//       "|"
-//     )}" -c copy -bsf:a aac_adtstoasc output.mp4`
-//   );
-// };
+const concat = async () => {
+  console.time("convert");
+  _FILES.forEach(async (file, index) => {
+    console.log(`🚀 (${index + 1} of ${_FILES.length})file: ${file}`);
+    await execSync(
+      `ffmpeg -i ${
+        _INPUT_FOLDER + file + ".MP4"
+      } -c copy -bsf:v h264_mp4toannexb -f mpegts ${_OUTPUT_FOLDER + file}.ts`
+    );
+  });
 
-// concat();
+  console.log(CONSOLE_YELLOW, "");
+  console.timeEnd("convert");
+  console.log(CONSOLE_RESET, "");
+
+  console.time("concat");
+  const filesList = _FILES
+    .map((file) => _OUTPUT_FOLDER + file + ".ts")
+    .join("|");
+  const command = `ffmpeg -i "concat:${filesList}" -c copy -bsf:a aac_adtstoasc ${_FINAL_RESULT}`;
+  console.log(
+    "🚀 ~ file: video-concat.js ~ line 34 ~ concat ~ command",
+    command
+  );
+  execSync(command);
+  console.log(CONSOLE_YELLOW, "");
+  console.timeEnd("concat");
+  console.log(CONSOLE_RESET, "");
+};
+
+concat();
