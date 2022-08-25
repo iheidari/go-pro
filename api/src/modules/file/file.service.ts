513238aaf11b96c3bb273712fe8410fb89d13079
Iman Heidari <iman.heydari@gmail.com>
2022-08-24 20:37:29 -0700

    fix api bug-STATIC_PATH

--- api/src/modules/file/file.service.ts
+++ api/src/modules/file/file.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { getAllFiles, File } from 'util/file';
-import { STATIC_PATH } from './file.module';
 
 @Injectable()
 export class FileService {
   getFiles(path: string): File[] {
-    return getAllFiles(path, '.mp4', [], STATIC_PATH);
+    return getAllFiles(path, '.mp4', [], process.env.STATIC_PATH);
   }
 }
